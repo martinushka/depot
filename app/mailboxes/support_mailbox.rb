e7512af4715e4cb934b77366355dcc5a5b8947c2
martinushka <martinushka536@yandex.ru>
2022-05-31 15:28:06 +0300

    support mailbox

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -2,11 +2,13 @@ class SupportMailbox < ApplicationMailbox
   def process
     recent_order = Order.where(email: mail.from_address.to_s).
       order('created_at desc').
-        first
+      first
+
     SupportRequest.create!(
       email: mail.from_address.to_s,
       subject: mail.subject,
       body: mail.body.to_s,
       order: recent_order
+    )
   end
 end
